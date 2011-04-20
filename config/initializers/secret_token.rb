83f4081976da8e357be262361991c79f4305a987
Lucas Franceschi <lukas1596@gmail.com>
2011-04-20 09:54:33 -0300

    Commit Inicial, apenas com o esqueleto da aplicacao rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SeminarioAlternativo::Application.config.secret_token = '86421cff689ca60edc94c753e536c971d340f9b6a0d111ce284104d448d1254520cc724077b36f939643c18302b841bbe63ede9c9ffe8a1bc64cb71dbe2c45aa'
